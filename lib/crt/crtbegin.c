95cfa5045546a6e4c5e1441382cc590d820eef28
Simon Gerber <simon.gerber@inf.ethz.ch>
2016-11-22 14:51:49 +0100

    week9 handout

--- lib/crt/crtbegin.c
+++ lib/crt/crtbegin.c
@@ -4,12 +4,12 @@
  */
 
 /*
- * Copyright (c) 2007, 2008, 2009, 2010, 2011, ETH Zurich.
+ * Copyright (c) 2007, 2008, 2009, 2010, 2011, 2016 ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
  * If you do not find this file, copies can be found by writing to:
- * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ * ETH Zurich D-INFK, Universitaetsstrasse 6, CH-8092 Zurich. Attn: Systems Group.
  */
 
 #include <stdint.h>
@@ -20,7 +20,7 @@ void *__dso_handle = &__dso_handle;
 typedef void (*CDtor)(void);
 
 /// First entry in .ctors array is the unspecified length marker (-1)
-static CDtor ctors[1]
+static volatile CDtor ctors[]
 __attribute__ ((unused, section(".ctors"), aligned(sizeof(CDtor))))
     = { (CDtor)(-1) };
 
